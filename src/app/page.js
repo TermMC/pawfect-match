3aa964fb5847feef798e380697462202704503b1
TermMC <termmc@gmail.com>
2024-09-30 11:15:25 +0100

    feat: do fetch for client and server components

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <Link href='/supabase-test'>Click Here To See Some User Data</Link>
+                <Link href='/supabase-server-test'>Click Here To See Some User Data In A Server Component</Link>
+                <Link href='/supabase-client-test'>Click Here To See Some User Data In A Client Component</Link>
             </main>
         </div>
     );
