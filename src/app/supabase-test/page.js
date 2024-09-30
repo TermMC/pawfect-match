b5bb21c6bf33032eebf2375dbec40583cd3cd4e4
TermMC <termmc@gmail.com>
2024-09-30 11:10:03 +0100

    feat:pull in user data

--- src/app/supabase-test/page.js
+++ src/app/supabase-test/page.js
@@ -5,12 +5,20 @@ export default async function Page() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
 
-    const { data: todos } = await supabase.from('todos').select();
+    const { data: users } = await supabase.from('account').select();
 
     return (
         <ul>
-            {todos?.map((todo) => (
-                <li>{todo}</li>
+            {users?.map((user, index) => (
+                <li key={index}>
+                    <div>
+                        <p>{user.name}</p>
+                        <p>{user.username}</p>
+                        <p>{user.email}</p>
+                        <p>{user.bio}</p>
+                    </div>
+                    <br />
+                </li>
             ))}
         </ul>
     );
