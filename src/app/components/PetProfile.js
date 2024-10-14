f7dfe65ff0c10399a61bc76d088f1fd8b13c674f
Naomi Briggs <nbriggs@redev.co.uk>
2024-10-14 13:31:01 +0100

    Linked to db

--- src/app/components/PetProfile.js
+++ src/app/components/PetProfile.js
@@ -7,24 +7,30 @@ import {
   faSquareXmark,
   faSquareCheck
 } from "@fortawesome/free-solid-svg-icons";
+import {supabase} from "@/utils/supabase/client";
 
-const PetProfile = () => {
+export default async function PetProfile() {
+  const { data: pet } = await supabase.from("pets")
+      .select('pet_id, name, breed, age, gender, vaccinated, description' )
+      .order('pet_id', { ascending: false })
+      .limit(1)
+      .single();
   return (
-    <div>
-      <Image src="https://place.dog/500/450" width="500" height="450" alt="Pet Image" ></Image>
+    <div key={pet.pet_id} className={styles.pageContainer}>
+      <Image src="https://place.dog/500/450" width="500" height="450" alt="Pet Image"></Image>
       <div className={styles.profileContainer}>
         <div className={styles.headerContainer}>
-          <h1>Bruce</h1>
-          <h2>Boxer</h2>
+          <h1>{pet.name}</h1>
+          <h2>{pet.breed}</h2>
         </div>
         <div className={styles.tagContainer}>
-          <Tag title="Age" description="2 years" />
-          <Tag title="Gender" description="Male" />
-          <Tag title="Vaccinated" description="Yes" />
+          <Tag title="Age" description={`${pet.age}`} />
+          <Tag title="Gender" description={`${pet.gender}`} />
+          <Tag title="Vaccinated" description={pet.vaccinated ? 'Yes' : 'No'} />
         </div>
         <ShelterContainer />
         <div className={styles.petDescription}>
-          <p>Lorem ipsum dolor sit amet consectetur. Magna sagittis quis amet egestas at egestas nunc ultricies.<a href="/profile">Read more...</a></p>
+          <p>{pet.description}<a href="/profile">Read more...</a></p>
         </div>
         <div className={styles.buttonContainer}>
         <FontAwesomeIcon icon={faSquareXmark} size="5x" style={{color: "#B32828"}} />
@@ -34,4 +40,3 @@ const PetProfile = () => {
     </div>
   );
 }
-export default PetProfile;
--- src/app/components/ShelterDetails.js
+++ src/app/components/ShelterDetails.js
@@ -4,19 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronRight
 } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const ShelterDetails = () => {
     return (
-        <div className={styles.shelterContainer}>
-          <Image styles={styles.shelterImage} src="/images/ShelterImage.svg" width="80" height="80" alt="Shelter logo"></Image>
-          <div className={styles.shelterDetails}>
-            <h1>Shelter Name</h1>
-            <h2>Location</h2>
-          </div>
-          <div className={styles.rightArrow}>
-          <FontAwesomeIcon icon={faChevronRight} size="2x" style={{color: "#D6D6D6"}} />
-          </div>
-        </div>
+        <Link href="/shelter">
+            <div className={styles.shelterContainer}>
+                <Image className={styles.shelterImage} src="/images/ShelterImage.svg" width="80" height="80"
+                       alt="Shelter logo"></Image>
+                <div className={styles.shelterDetails}>
+                    <h1>Shelter Name</h1>
+                    <h2>Location</h2>
+                </div>
+                <div className={styles.rightArrow}>
+                    <FontAwesomeIcon icon={faChevronRight} size="2x" style={{color: "#D6D6D6"}}/>
+                </div>
+            </div>
+        </Link>
     );
 }
 export default ShelterDetails;
