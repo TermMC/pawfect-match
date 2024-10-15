53316116c69525af16e703d6c0a6a4b98e6326d5
bhunt1309 <bhunt485@gmail.com>
2024-10-15 15:47:04 +0100

    labels and data into form

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,32 +1,42 @@
+'use client'
+import React from "react"
 import "./UserProfile.css"
 import Image from "next/image"
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import { supabase } from "@/utils/supabase/client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default async function Page() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+const{data: account} = await supabase.from("account")
+.select('user_id, username, email, name, date_of_birth, bio, verified')
+.limit(1)
+.single();
+const handleSubmit = async (e) =>{
+
+    e.preventDefault()
+}
 
   return (
         <>
         <section className="profilePageHolder">
         <div className='titleBox'>
-            <div className = "profTitle">Hey, Username! &#128075;</div>
+            <div className = "profTitle">Hey, {account.username}! &#128075;</div>
         </div>
         <div className = "profPictureBox">
             <Image src="/images/Avatar.png" className="profPicture" width='200' height ='200'></Image>
         </div>
         <div className ='fbox'>
-         <form id="pform">
+         <form id="pform" onSubmit={handleSubmit}>
             <fieldset>
-            <input type="text" name="fname" placeholder="First Name" />
-            <input type="text" name="sname" placeholder="Surname" />
-            <input type="text" name="age" placeholder="Age" />
-            <textarea id='biofield' rows='10' cols='100' placeholder="Tell us more about you..."  />
-            <input type="button" name="next" className="updateButton" value="Update"/>
+                <label htmlFor="name">Name</label>
+                <input  id="name" type="text" name="name" placeholder="Full Name" defaultValue={account.name}/>
+                <label htmlFor="dob">Date of Birth</label>
+                <input  type="date" name="dob" id="dob" defaultValue={account.date_of_birth} />
+                <label htmlFor="biofield">Bio</label>
+                <textarea id='biofield' rows='10' cols='100' defaultValue={account.bio}  />
+                <input type="button" name="next" className="updateButton" value="Update"/>
             </fieldset>
-            </form>
+        </form>
         </div>
         </section>
         
