ff49da2f7870bbd5402cc8d0a9204dcb0980cf6d
Naomi Briggs <nbriggs@redev.co.uk>
2024-10-31 10:46:46 +0000

    Updated styling on login page

--- src/app/error/page.js
+++ src/app/error/page.js
@@ -1,3 +1,4 @@
 export default function ErrorPage() {
-    return <p>Sorry, something went wrong</p>
+    return (
+        <p>Sorry, something went wrong. <a href="/login"><strong>Return to login</strong></a></p>)
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,31 @@
 import { login, signup } from './actions'
+import './login.css'
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPaw
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function LoginPage() {
     return (
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" required />
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="password" required />
-            <button formAction={login}>Log in</button>
-            <button formAction={signup}>Sign up</button>
+
+        <form className="loginPage">
+            <Image className="loginLogo" src="./images/Logo.svg" alt="Pawfect Match logo" width={200} height={200} />
+            <div className="loginContainer">
+                <div className="loginDetailsContainer">
+                    <div className="emailContainer">
+                        <label htmlFor="email">Email</label>
+                        <input id="email" name="email" type="email" required/>
+                    </div>
+                    <div className="passwordContainer">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" name="password" type="password" required/>
+                    </div>
+                    <button className="loginButton" formAction={login}>Log in</button>
+                    <button className="signupButton" formAction={signup}>Sign up</button>
+                </div>
+                <FontAwesomeIcon icon={faPaw} size="8x" style={{color: "#002846", position: "absolute", bottom: 0, right: 0, transform: "rotate(-45deg)" }}/>
+            </div>
         </form>
     )
 }
