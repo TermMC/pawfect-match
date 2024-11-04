5b297a0cf77a5f9109a4043f24357c7e1ee171c7
bhunt1309 <bhunt485@gmail.com>
2024-11-04 11:36:10 +0000

    added like and back buttons, general clean up

--- src/app/(protected)/pet-profile/page.js
+++ src/app/(protected)/pet-profile/page.js
@@ -4,12 +4,13 @@ import './PetProfilePage.css';
 import ShelterContainer from "../../components/ShelterDetails";
 import {supabase} from "@/utils/supabase/client";
 import SmallTag from '@/app/components/SmallTag';
-import { faVenusMars, faUpRightAndDownLeftFromCenter, faSyringe, faMicrochip, faChild, faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faVenusMars, faUpRightAndDownLeftFromCenter, faSyringe, faMicrochip, faChild, faPaw, faHeart, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default async function PetProfile() {
   const { data: pet } = await supabase.from("pets")
-      .select('pet_id, name, breed, age, gender, vaccinated, description, microchip, child_friendly, pet_friendly, size' )
+      .select('pet_id, name, breed, age, gender, vaccinated, long_description, microchip, child_friendly, pet_friendly, size' )
       .order('pet_id', { ascending: false })
       .limit(1)
       .single();
@@ -18,11 +19,10 @@ export default async function PetProfile() {
     <div className='petName'>
       <div className='petDetails'>
         <h1>{pet.name}</h1>
-        <h2>{pet.breed}</h2>
-        <h2>{pet.age} years old</h2>
+        <p>{pet.breed}</p>
+        <p>{pet.age} years old</p>
         </div>
       <div className="petImageContainer">
-        <Image className="petImage" src="https://place.dog/500/550" width="300" height="350" alt="Pet Image"></Image>
       </div>
     </div>
     <div className='petInfo'>
@@ -33,12 +33,19 @@ export default async function PetProfile() {
       <SmallTag title={pet.child_friendly? 'Child-friendly' : 'Not Child-friendly'} icon= {faChild}/>
       <SmallTag title={pet.pet_friendly? 'Fine with other Animals' : 'Not Pet-friendly'} icon= {faPaw}/>
     </div>
-    <ShelterContainer />
     <div className='petDescription'>
       <h2>More about me...</h2>
-      <p>{pet.description}</p>
+      <p>{pet.long_description}</p>
     </div>
-    
+    <ShelterContainer className='shelterContainer' />
+    <div className='buttonSectionContainer'>
+      <div className='backButtonContainer'>
+        <a href='/'><FontAwesomeIcon className='backButton' icon={faArrowLeft}/></a>
+      </div>
+      <div className='buttonContainer'>
+        <button className='adoptButton'><FontAwesomeIcon className='fa-icon' icon={faHeart}/></button>
+      </div>
+    </div>
   </>
   )
 }
