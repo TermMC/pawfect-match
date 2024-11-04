e66d21d70cc65e7fd204ea8ff6cefe2fe77022f0
Naomi Briggs <nbriggs@redev.co.uk>
2024-11-04 16:33:06 +0000

    linked home page to correct shelter

--- src/app/components/ShelterDetails.js
+++ src/app/components/ShelterDetails.js
@@ -5,23 +5,37 @@ import {
   faChevronRight
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import {supabase} from "@/utils/supabase/client";
+import {cookies} from "next/headers";
+import {createClient} from "@/utils/supabase/server";
 
+export default async function ShelterDetails({ petId, shelterId }) {
+    const cookieStore = cookies();
+    const supabase = await createClient(cookieStore);
 
-
-export default async function ShelterDetails() {
-    const {data: shelters} = await supabase.from("shelters")
-        .select('shelter_id, name')
+    const { data: pet, error} = await supabase.from('pets')
+        .select(`owner_id, pet_id, shelters(shelter_id, name, location)`)
+        .eq('pet_id', petId)
         .limit(1)
         .single();
+
+    if (error) {
+        console.error(error);
+        return <div>Error fetching shelter details.</div>
+    }
+
+    const shelter = pet.shelters;
+    if (!shelter) {
+        return <div>No shelter information available.</div>;
+    }
+
     return (
-        <Link href="/shelter">
+        <Link href={`/shelter/${shelterId}`}>`
             <div className="shelterDetailsContainer">
                 <Image className="shelterDetailsImage" src="/images/ShelterImage.svg" width="80" height="80"
                        alt="Shelter logo"></Image>
                 <div className="shelterDetails">
-                    <h1>{shelters.name}</h1>
-                    <h2>Location</h2>
+                    <h1>{shelter.name}</h1>
+                    <h2>{shelter.location}</h2>
                 </div>
                 <div className="rightArrow">
                     <FontAwesomeIcon icon={faChevronRight} size="2x" style={{color: "#D6D6D6"}}/>
