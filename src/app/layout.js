db1bc528a818466ede35b62dd50c5747f015439f
Naomi Briggs <nbriggs@redev.co.uk>
2024-11-05 10:46:25 +0000

    readded Poppins as font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
 import './globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-// import { Poppins } from '@next/font/google';
+import { Poppins } from 'next/font/google';
 
-// const poppins = Poppins({
-//     subsets: ['latin'],
-//     display: 'swap',
-//     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-// });
+const poppins = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 config.autoAddCss = false;
 
