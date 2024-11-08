992f938f725300a40c7ce7d2ce10bf8c899fd529
TermMC <termmc@gmail.com>
2024-11-08 15:43:05 +0000

    feat: neat timestamp

--- src/app/components/ConversationMessageCard.js
+++ src/app/components/ConversationMessageCard.js
@@ -1,11 +1,22 @@
 import './ConversationMessageCard.css';
 
+function formatTimestamp(sentAt) {
+    const date = new Date(sentAt);
+    return new Intl.DateTimeFormat('en-US', {
+        day: 'numeric',
+        month: 'short',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true, // Displays time in AM/PM format
+    }).format(date);
+}
+
 export default function ConversationMessageCard({ bodyText, sentAt, sender }) {
     return (
         <div className={sender == 'user' ? 'user-message-card message-card' : 'shelter-message-card message-card'}>
             <div className='message-card-content'>
                 <p className='message-card-body' dangerouslySetInnerHTML={{ __html: bodyText }}></p>
-                <p className='message-card-time'> {sentAt}</p>
+                <p className='message-card-time'> {formatTimestamp(sentAt)}</p>
             </div>
             <div></div>
         </div>
