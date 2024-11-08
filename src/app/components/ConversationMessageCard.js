a716240de321ad58e48cfcd5473c979a1ea26c22
TermMC <termmc@gmail.com>
2024-11-08 15:34:59 +0000

    feat: styling and message send for conversation

--- src/app/components/ConversationMessageCard.js
+++ src/app/components/ConversationMessageCard.js
@@ -2,7 +2,7 @@ import './ConversationMessageCard.css';
 
 export default function ConversationMessageCard({ bodyText, sentAt, sender }) {
     return (
-        <div className='message-card'>
+        <div className={sender == 'user' ? 'user-message-card message-card' : 'shelter-message-card message-card'}>
             <div className='message-card-content'>
                 <p className='message-card-body' dangerouslySetInnerHTML={{ __html: bodyText }}></p>
                 <p className='message-card-time'> {sentAt}</p>
