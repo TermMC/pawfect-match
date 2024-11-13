ba9c91737b275b88afb8a82248976a787be9c54d
Naomi Briggs <nbriggs@redev.co.uk>
2024-11-13 15:31:59 +0000

    Added logout to navbar

--- src/app/components/navBar.js
+++ src/app/components/navBar.js
@@ -3,10 +3,9 @@ import React, {useState} from "react";
 import './NavBar.css'
 import Image from "next/image";
 import Link from "next/link";
-
+import {supabase} from "@/utils/supabase/client";
 
 const Navbar = () => {
-
     // to change burger classes
     const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
     const [menu_class, setMenuClass] = useState("menu hidden")
@@ -14,31 +13,55 @@ const Navbar = () => {
 
     // toggle burger menu change
     const updateMenu = () => {
-        if(!isMenuClicked) {
+        if (!isMenuClicked) {
             setBurgerClass("burger-bar clicked")
             setMenuClass("menu visible")
-        }
-        else {
+        } else {
             setBurgerClass("burger-bar unclicked")
             setMenuClass("menu hidden")
         }
         setIsMenuClicked(!isMenuClicked)
     }
 
-    return(
+    // Logout button
+    const HandleLogout = async () => {
+        try {
+            const {error} = await supabase.auth.signOut()
+
+            if (error) {
+                console.log('Error logging out:', error.message);
+                window.alert()
+            }
+            else {
+                console.log('User logged out');
+            }
+        }
+        catch (err) {
+            console.log('Error logging out:', err);
+            window.alert("Error when logging out, try again.");
+        }
+    }
+
+    async function signOut() {
+        const { error } = await supabase.auth.signOut()
+    }
+
+
+    return (
         <div style={{width: '100%'}}>
             <nav>
-              <div>
-                  <Image className="logo" src="/images/Logo.svg" width={200} height={80} alt="Pawfect Match logo" />
-              </div>
-              <Link href="/" className="desktop_menu">Home</Link>
-              <Link href="/profile" className="desktop_menu">Profile</Link>
-              <Link href="/messages" className="desktop_menu">Messages</Link>
-              <Link href="/matches" className="desktop_menu">Matches</Link>
+                <div>
+                    <Image className="logo" src="/images/Logo.svg" width={200} height={80} alt="Pawfect Match logo"/>
+                </div>
+                <Link href="/" className="desktop_menu">Home</Link>
+                <Link href="/profile" className="desktop_menu">Profile</Link>
+                <Link href="/messages" className="desktop_menu">Messages</Link>
+                <Link href="/matches" className="desktop_menu">Matches</Link>
+                <Link href="/login" onClick={HandleLogout} className="desktop_menu">Logout</Link>
                 <div className="burger-menu" onClick={updateMenu}>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
                 </div>
             </nav>
 
@@ -48,6 +71,7 @@ const Navbar = () => {
                     <li><Link href="/profile">Profile</Link></li>
                     <li><Link href="/messages">Messages</Link></li>
                     <li><Link href="/matches">Matches</Link></li>
+                    <li><Link href="/login" onClick={HandleLogout}>Logout</Link></li>
                 </ul>
             </div>
         </div>
