2a7ef677bfa0be135d8afb60def2c1874ecdc4f0
Elliot F-A <7229285+TermMC@users.noreply.github.com>
2024-11-20 21:56:36 +0000

    fix: suspense wrapper moved to separate component

--- src/app/error/page.js
+++ src/app/error/page.js
@@ -2,17 +2,19 @@
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-export default function ErrorPage() {
-    const searchParams = useSearchParams()
+function ErrorMessage() {
+ const searchParams = useSearchParams()
     const errorCode = searchParams.get('error') | 0;
     const errorMessages = [
         "Sorry, something went wrong.", "Username already taken please sign up again with a different usename."
     ]
-    console.log(errorCode, typeof errorCode, errorMessages[errorCode])
+    return <p>{errorMessages[errorCode]}</p>
+}
 
+export default function ErrorPage() {
     return (
         <div>
-            <Suspense><p>{errorMessages[errorCode]}</p></Suspense>
+            <Suspense><ErrorMessage/></Suspense>
             <p><a href="/login"><strong>Return to login</strong></a></p>
         </div>)
 }
